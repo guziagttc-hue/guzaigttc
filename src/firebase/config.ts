bcbc0c39b50c90091c31fa402681a8fdf7cfec71
guzia gttc <guziagttc@gmail.com>
2026-01-20 04:27:18 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCw6JexNNj8uHVfh5LZpu10D-Bj0aWn

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyCw6JexNNj8uHVfh5LZpu10D-Bj0aWnHjE",
   authDomain: "studio-8990543519-d9691.firebaseapp.com",
+  databaseURL: "https://studio-8990543519-d9691-default-rtdb.firebaseio.com",
   projectId: "studio-8990543519-d9691",
-  storageBucket: "studio-8990543519-d9691.appspot.com",
+  storageBucket: "studio-8990543519-d9691.firebasestorage.app",
   messagingSenderId: "873123350814",
   appId: "1:873123350814:web:0e1d9bab97adb505f5e10f"
 };
